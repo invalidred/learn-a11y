5baa0fed53481842599b2969b3c8e76f7e2694b9
Abdul Khan <x210812@L600045.local>
2018-08-21 21:00:21 -0400

    tab trapping proper

--- js/modal.js
+++ js/modal.js
@@ -8,11 +8,44 @@ cancelButton.addEventListener('click', close);
 
 // Get a list of tabbable elements here:
 // https://github.com/jkup/focusable
+const focusableElementsRaw = modal.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary')
+const focusableElements = [...focusableElementsRaw]
+const firstElement = focusableElements[0]
+const lastElement = focusableElements[focusableElements.length - 1]
 
 function open() {
   // Show the modal and overlay
+  const previouslyActive = document.activeElement
+  modal.addEventListener('keydown', trap)
+  modal.addEventListener('keydown', closeOnEscacpe)
   modal.style.display = 'block';
   modalOverlay.style.display = 'block';
+
+  function closeOnEscacpe(event) {
+    const { key } = event
+    if (key === 'Escape') {
+      close()
+    }
+  }
+
+  function trap(event) {
+    const { key } = event
+    const { activeElement } = document
+
+    if (key !== 'Tab') {
+      return
+    }
+
+    if (event.shiftKey && activeElement === firstElement) {
+      event.preventDefault()
+      focusableElements[focusableElements.length - 1].focus()
+    }
+
+    if (!event.shiftKey && activeElement === lastElement) {
+      event.preventDefault()
+      focusableElements[0].focus()
+    }
+  }
 }
 
 function close() {
